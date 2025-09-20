649644c3ab55ec44b4a9ab0962db76483ab5be35
Muhamad Hafiz Saputra <muhamadhafizsaputra1@gmail.com>
2025-09-20 13:26:43 +0700

    Make cart quantity interactive

--- script/amazon.js
+++ script/amazon.js
@@ -99,7 +99,14 @@ document.querySelectorAll('.button-js').forEach((button) => {
           quantity: 1
         })
       }
-    console.log(cart);
+    
+    let cartQuantity=0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   })
 })
 
